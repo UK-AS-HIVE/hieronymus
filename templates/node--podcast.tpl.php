f407f37f4caa81946c1211717dc30120d9e8b6c2
Will Hieronymus <will.hieronymus01@gmail.com>
2015-11-19 15:03:52 -0500

    Added and styled links on profile pages

--- templates/node--podcast.tpl.php
+++ templates/node--podcast.tpl.php
@@ -1,5 +1,6 @@
 <?php
   hide($content['field_embed']);
+  hide($content['field_audio']);
 ?>
 
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
@@ -20,7 +21,23 @@
   <?php endif; ?>
 
   <div class="content clearfix"<?php print $content_attributes; ?>>
-    <?php
+    <?
+      if(!empty($content['field_audio']['#items'])){
+      	print render($content['field_image2']);
+        drupal_add_css('//cdn.as.uky.edu/third-party/mediaelement/mediaelementplayer.min.css');
+        drupal_add_js('//cdn.as.uky.edu/third-party/mediaelement/mediaelement-and-player.min.js', 'external');
+        $url = file_create_url($content['field_audio']['#items'][0]['uri']);
+        print '<audio id="podcast-' . $node->nid . '" src="' . $url . '" type="audio/mp3" controls="controls"></audio>';
+        print '<script type="text/javascript">
+          <!--//--><![CDATA[//><!-- 
+            (function($) {
+              $(\'#podcast-' . $node->nid . '\').mediaelementplayer({
+                audioWidth:"100%"
+              });
+            }(jQuery));
+        //--><!]]>
+          </script>';
+      }
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
       hide($content['links']);
